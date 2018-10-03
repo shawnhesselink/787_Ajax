21691a311ed5a02fc571437de7b5a755f880b57b
Shawn Hesselink <shawn.hesselink@gmail.com>
2018-10-03 17:52:57 -0400

    nav highlight fix

--- Module_5/js/script.js
+++ Module_5/js/script.js
@@ -53,11 +53,35 @@ var switchMenuToActive = function () {
   classes = classes.replace(new RegExp("active", "g"), "");
   document.querySelector("#navHomeButton").className = classes;
 
+  // Remove 'active' from about button
+  var classes = document.querySelector("#navAboutButton").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector("#navAboutButton").className = classes;
+
   // Add 'active' to menu button if not already there
   classes = document.querySelector("#navMenuButton").className;
   if (classes.indexOf("active") === -1) {
     classes += " active";
     document.querySelector("#navMenuButton").className = classes;
+  }
+};
+
+var switchAboutToActive = function () {
+  // Remove 'active' from home button
+  var classes = document.querySelector("#navHomeButton").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector("#navHomeButton").className = classes;
+
+  // Remove 'active' from about button
+  var classes = document.querySelector("#navMenuButton").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector("#navMenuButton").className = classes;
+
+  // Add 'active' to menu button if not already there
+  classes = document.querySelector("#navAboutButton").className;
+  if (classes.indexOf("active") === -1) {
+    classes += " active";
+    document.querySelector("#navAboutButton").className = classes;
   }
 };
 
@@ -170,6 +194,13 @@ function buildAndShowAbout (about) {
   } else {
     about = insertProperty(about, "five", noStar);
   }
+  // $ajaxUtils.sendGetRequest(
+  //   aboutHtml,
+  //   function () {
+      // Switch CSS class active to menu button
+      switchAboutToActive();
+    // },
+    // false);
 
   about = insertProperty(about, "number", rando);
   insertHtml("#main-content", about);
